db0e33e7c6ef57cca6c68e75537c27ec2af01a2c
20051710 <aksharmandaviya245@gmail.com>
2025-06-12 17:46:19 +0000

    learn objects

---
+++ 09_objects.js
@@ -0,0 +1,37 @@
+// singleton
+// constructer
+
+// Object.create
+
+//  object literals
+
+
+const mysy = Symbol("key1")
+
+const js_user = {
+    name : "akshar",
+    age:18,
+    location:"surat",
+    [mysy]:'mykey1',
+    "full Name":"akshar mandaviya"
+}
+
+// console.log(js_user.age)
+// console.log(js_user["age"])
+
+// console.log(js_user.full Name)  not work   not access use dot
+// console.log(js_user["full Name"])
+
+// console.log(typeof js_user[mysy])
+
+
+// Object.freeze(js_user)   not allow to chnages
+
+
+
+
+js_user.greeting = function(){
+    console.log("hello")
+}
+
+console.log(js_user.greeting)

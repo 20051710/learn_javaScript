8e51fa256c83be6c2c2bf948adffd541952ca517
20051710 <aksharmandaviya245@gmail.com>
2025-06-12 17:29:11 +0000

    array aedvanced

---
+++ 08_array2.js
@@ -0,0 +1,24 @@
+const marval_heros = ["spiderman","thor","hulk"]
+const dc_heros = ["superMAn","batman"]
+
+// marval_heros.push(dc_heros)
+
+// console.log(marval_heros)
+
+const all_heros = marval_heros.concat(dc_heros)
+
+console.log(all_heros)
+
+// const  all_new_heros = [...marval_heros,...dc_heros]
+// console.log(all_new_heros)   spread opretore
+
+// const new_arr  = [1,22,3,5,[1,2,3],5,5,8,[7,8,9],8,8,8]
+
+// console.log(new_arr)
+
+// const other_new = new_arr.flat()  all concate the array
+
+// console.log(other_new)
+
+
+console.log(Array.from("akshar"))
